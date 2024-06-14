da5abac31edafdc1affd376e5c053ba681a856b9
mohamed sahir <msahirt@gmail.com>
2024-06-14 07:47:35 +0530

    backend commit with payment files

--- Models/paymentModel.js
+++ Models/paymentModel.js
@@ -1,31 +1,6 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-    // paymentID: {
-    //     type: String,
-    //     required: true,
-    //     maxLength: 50,
-    // },
-    // totalCost: {
-    //     type: Number,
-    //     required: true,
-    // },
-    // paymentDate: {
-    //     type: Date,
-    //     default: Date.now(),
-    // },
-    // paymentMethod: {
-    //     type: String,
-    //     required: true,
-    // },
-    // status: {
-    //     type: Number,
-    //     default: 1,
-    //     // 1 started,
-    //     // 2 successfull,
-    //     // 3 failed,
-    //     // 4 refund
-    // },
 
     transactionId: { 
         type: String, 
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,5 +1,4 @@
 import express from "express"
-// import { orders, verify } from "../Controllers/paymentController.js";
 import { createPayment, PaymentToken } from "../Controllers/paymentController.js";
 import { userAuth } from "../middlewares/authorization.js";
 const paymentRouter = express.Router();
